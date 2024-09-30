6fcef8c67befb31bde27fa9b078ba06659e0d80c
HatsuSixty <hatsunotsus@gmail.com>
2024-09-30 16:36:03 -0300

    [server] Handle close button

--- src/WindowRenderer/wm.c
+++ src/WindowRenderer/wm.c
@@ -5,6 +5,7 @@
 #include "input.h"
 #include "input_events/mouse.h"
 #include "server/server.h"
+#include "log.h"
 
 static bool check_collision_point_rec(Vector2 point, Vector2 rec_position, Vector2 rec_size)
 {
@@ -29,15 +30,18 @@ WMWindowParameters wm_compute_window_parameters(Window* window)
     const float border_thickness = 5.f;
     const float title_bar_thickness = 20.f;
 
-    const Vector2 title_bar_position = { window->x, window->y };
-    const Vector2 title_bar_size = {
-        .x = window->width + border_thickness * 2.f,
+    const Vector2 close_button_position = { window->x, window->y };
+    const Vector2 close_button_size = {
+        .x = title_bar_thickness,
         .y = title_bar_thickness,
     };
 
-    const Vector2 close_button_position = title_bar_position;
-    const Vector2 close_button_size = {
-        .x = title_bar_thickness,
+    const Vector2 title_bar_position = {
+        .x = close_button_position.x + close_button_size.x,
+        .y = close_button_position.y,
+    };
+    const Vector2 title_bar_size = {
+        .x = window->width - close_button_size.x + border_thickness * 2.f,
         .y = title_bar_thickness,
     };
 
@@ -87,31 +91,42 @@ void wm_update(Server* server, Vector2 cursor_position)
      */
     server_lock_windows(server);
 
-    // Handle window dragging
+    // Handle window dragging/close button
     {
         for (size_t i = 0; i < server_get_window_count(server); ++i) {
             Window* window = server_get_windows(server)[i];
             bool window_is_active = window->id == server_top_window(server)->id;
             WMWindowParameters window_parameters = wm_compute_window_parameters(window);
 
-            if (check_collision_point_rec(cursor_position,
-                                          window_parameters.title_bar_position,
-                                          window_parameters.title_bar_size)
-                && is_mouse_button_just_pressed(INPUT_MOUSE_BUTTON_LEFT)) {
-                WM.dragged_window_id = window->id;
-                WM.drag_offset.x = cursor_position.x - window->x;
-                WM.drag_offset.y = cursor_position.y - window->y;
+            // Handle window dragging
+            {
+                if (check_collision_point_rec(cursor_position,
+                                              window_parameters.title_bar_position,
+                                              window_parameters.title_bar_size)
+                    && is_mouse_button_just_pressed(INPUT_MOUSE_BUTTON_LEFT)) {
+                    WM.dragged_window_id = window->id;
+                    WM.drag_offset.x = cursor_position.x - window->x;
+                    WM.drag_offset.y = cursor_position.y - window->y;
+                }
+
+                if (WM.dragged_window_id == window->id) {
+                    if (window_is_active) {
+                        window->x = cursor_position.x - WM.drag_offset.x;
+                        window->y = cursor_position.y - WM.drag_offset.y;
+                    }
+
+                    if (is_mouse_button_just_released(INPUT_MOUSE_BUTTON_LEFT)) {
+                        WM.dragged_window_id = -1;
+                    }
+                }
             }
 
-            if (WM.dragged_window_id == window->id) {
-                if (window_is_active) {
-                    window->x = cursor_position.x - WM.drag_offset.x;
-                    window->y = cursor_position.y - WM.drag_offset.y;
-                }
-
-                if (is_mouse_button_just_released(INPUT_MOUSE_BUTTON_LEFT)) {
-                    WM.dragged_window_id = -1;
-                }
+            // Handle close button
+            if (check_collision_point_rec(cursor_position,
+                                          window_parameters.close_button_position,
+                                          window_parameters.close_button_size)
+                && is_mouse_button_just_pressed(INPUT_MOUSE_BUTTON_LEFT)) {
+                log_log(LOG_INFO, "Close button of window ID %d clicked!", window->id);
             }
         }
     }
